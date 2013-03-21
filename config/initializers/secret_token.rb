49f08d677a2c97817a861a25fa28f4ec7799c73f
Aditya <adityagarg.work@gmail.com>
2013-03-21 16:22:15 +0530

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscountParkRide::Application.config.secret_token = 'd3494abd9807e551c0be0898af89dd75ebb00b0e66985575c2986f7634043d28e601cd740ef4041b7f80a71a997f5a4fdfb2e23eceb6f993c53433ea50916546'
